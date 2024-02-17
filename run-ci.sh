ca73feb9b0d153468960af341e5e0a4a3b39a3ff
Sönke Ludwig <sludwig@outerproduct.org>
2024-02-17 09:38:39 +0100

    Fix macOS CI job by pre-selecting the OpenSSL version.

---
+++ run-ci.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e -x -o pipefail
+
+DCVER=${DC#*-}
+DC=${DC%-*}
+if [ "$DC" == "ldc" ]; then DC="ldc2"; fi
+DUB_FLAGS="${DUB_FLAGS:-} --compiler=$DC"
+
+
+# Check for trailing whitespace"
+grep -nrI --include='*.d' '\s$' . && (echo "Trailing whitespace found"; exit 1)
+
+# test for successful release build
+dub build -b release $DUB_FLAGS
+
+# test for successful 32-bit build
+if [ "$DC" == "dmd" ]; then
+	dub build --arch=x86 $DUB_FLAGS
+fi
+
+dub test $DUB_FLAGS
+
+if [ "$OS" == "macOS-latest" ]; then
+    dub test :tls -c openssl-1.1 $DUB_FLAGS
+else
+    dub test :tls $DUB_FLAGS
+fi
