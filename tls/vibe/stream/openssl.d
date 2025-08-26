7d4032eeac692c4aa4b0fa3d76e1eceeaea06461
Steven Schveighoffer <schveiguy@gmail.com>
2025-08-26 11:18:56 -0400

    Clean up keylog file.

--- tls/vibe/stream/openssl.d
+++ tls/vibe/stream/openssl.d
@@ -1263,13 +1263,13 @@ static if (haveKeylog) {
 	 * This must be called on every SSLContext you wish to log the key. If the
 	 * environment variable is not set, this does not log the key.
 	 */
+	private FileStream keyfile;
 	void keylogOnEnvVar(SSLContext* context) {
 		static extern(C) void callback(const SSL* ssl, const char* line) {
 			import vibe.core.file;
 			if (line is null) return;
 
 			// use a separate boolean to avoid always checking for an env var when it doesn't exist.
-			static FileStream keyfile;
 			static bool initialized;
 			if (!initialized) {
 				initialized = true;
@@ -1286,6 +1286,10 @@ static if (haveKeylog) {
 		}
 
 		context.keylogCallback = &callback;
+	}
+
+	static ~this() {
+		keyfile.close();
 	}
 }
 
